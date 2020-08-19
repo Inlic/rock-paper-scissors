a5742bac9f713893a16994c98b2ae0682ee6efea
Ben W <bwiedeback@gmail.com>
2020-08-19 16:57:52 -0600

    Updated and functional

--- app.js
+++ app.js
@@ -1,9 +1,74 @@
 let choices = [
-  {id: "rock"
-    scissor: "Tie",
-    paper: "Win",
-    rock: "Lose",
-    img: ""
-  
-  }
+  {id: "Rock",
+   outcomes: [ 
+      {Scissors: "Win"},
+      {Paper: "Lose"},
+      {Rock: "Tie"}
+   ],
+   img: "/assets/rock.jpg"
+  },
+  {id: "Paper",
+   outcomes: [ 
+    {Scissors: "Lose"},
+    {Paper: "Tie"},
+    {Rock: "Win"}
+   ],
+   img: "/assets/paper.jpg"
+  },
+  {id: "Scissors",
+   outcomes: [ 
+    {Scissors: "Tie"},
+    {Paper: "Win"},
+    {Rock: "Lose"}
+   ],
+  img: "/assets/scissors.jpg"
+ }
 ]
+
+function drawDefault(){
+  document.getElementById("player-image").innerHTML = "<img src='/assets/default.jpg'><h1 class='text-secondary'>Waiting on Player<h1>"
+  document.getElementById("ai-image").innerHTML = "<img src='/assets/default.jpg'>"
+}
+
+function drawButton(){
+  let template = ""
+  for(let i = 0; i < choices.length; i++){
+    let choice = choices[i]
+    template += `<button class="btn btn-primary mx-3 text-white" onclick="drawPlayer('${choice.id}')">${choice.id}</button>`
+  }
+  document.getElementById("player-choice").innerHTML = template
+}
+
+function drawPlayer(playerChoice){
+  let playerIndex = choices.findIndex(c => c.id == playerChoice)
+  let playerImage = choices[playerIndex].img
+  document.getElementById("player-image").innerHTML = `<img class='animate' src='${playerImage}'><h1 class='text-secondary'>Player chooses ${playerChoice}</h1>`
+  drawComputer(playerIndex)  
+}
+
+function drawComputer(playerIndex){
+  let aiChoice = choices[playerIndex].outcomes[Math.floor(Math.random()*choices[playerIndex].outcomes.length)]
+  let aiList = Object.entries(aiChoice)
+  let aiID = aiList['0'][0]
+  let gameStatus = aiList['0'][1]
+  let aiIndex = choices.findIndex(c => c.id == aiID)
+  let aiImage = choices[aiIndex].img
+  document.getElementById("ai-image").innerHTML = `<img class='animate' src='${aiImage}'><h1 class='text-white animate'>Computer chooses ${aiID}</h1>`
+  document.getElementById("matchup-outcome").innerHTML = `<h1 class = 'animate' >${gameStatus}</h1>`
+}
+
+function drawInitial(){
+  drawDefault()
+  drawButton()
+}
+
+
+//draw the initial board
+// default image for player
+// buttons for player rock paper scissors
+// default image for computer choice
+// computer plays after player
+// rand function for computer choice 0 choices.length
+// message once computer chooses
+
+drawInitial()
